77ebf3e0e2fee2b83348defae6a26734e2c74f3a
jihwankim128 <jihwan1535@nate.com>
2026-01-03 20:38:17 +0900

    fix: 근무자 교체 로직 버그 해결

--- src/main/java/oncall/model/Oncall.java
+++ src/main/java/oncall/model/Oncall.java
@@ -5,6 +5,7 @@ public class Oncall {
     private final Workers holidayWorkers;
     private Worker lastWeekdayWorker;
     private Worker lastHolidayWorker;
+    private Worker lastWorker;
 
     public Oncall(Workers weekdayWorkers, Workers holidayWorkers) {
         this.weekdayWorkers = weekdayWorkers;
@@ -13,23 +14,25 @@ public class Oncall {
 
     public Worker getNextHolidayWorker() {
         Worker nextWorker = holidayWorkers.getNextWorker(lastHolidayWorker);
-        if (nextWorker.equals(lastWeekdayWorker)) {
+        if (nextWorker.equals(lastWorker)) {
             holidayWorkers.swapNext(nextWorker);
             nextWorker = holidayWorkers.getNextWorker(lastHolidayWorker);
         }
 
         lastHolidayWorker = nextWorker;
+        lastWorker = nextWorker;
         return nextWorker;
     }
 
     public Worker getNextWeekdayWorker() {
         Worker nextWorker = weekdayWorkers.getNextWorker(lastWeekdayWorker);
-        if (nextWorker.equals(lastHolidayWorker)) {
+        if (nextWorker.equals(lastWorker)) {
             weekdayWorkers.swapNext(nextWorker);
-            nextWorker = weekdayWorkers.getNextWorker(lastHolidayWorker);
+            nextWorker = weekdayWorkers.getNextWorker(lastWeekdayWorker);
         }
 
         lastWeekdayWorker = nextWorker;
+        lastWorker = nextWorker;
         return nextWorker;
     }
 }
